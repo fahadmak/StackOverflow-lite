cac636d363fad9243735b6754d347d809d069644
faraqhan <f.faraqhan@gmail.com>
2018-08-16 17:05:59 +0300

    add get_all_question method

--- api/model.py
+++ api/model.py
@@ -1,15 +1,20 @@
-class Question(object):
+class Questions(object):
     ques_id = 0
-    questions = [] 
+    questions: list = [] 
 
     def __init__(self, title):
         self.question = {}
         self.question['title'] = title
-        self.question['id_no'] += 1
+        Questions.ques_id += 1
+        self.question['id_no'] = Questions.ques_id
+        Questions.questions.append(self.questions)
         
     @staticmethod
     def get_all_questions(questions):
-        pass
+        if len(Questions.questions) == 0:
+            raise ValueError("No questions!")
+        else:
+            return Questions.questions
         
 
             
