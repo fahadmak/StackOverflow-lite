353400e5e25d9ab1bb1b11cf49eb41dcaef201cf
faraqhan <f.faraqhan@gmail.com>
2018-08-17 22:30:32 +0300

    add tests for the app get_question_byId_function

--- test_app.py
+++ test_app.py
@@ -9,13 +9,22 @@ class QuestionTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-    
+        self.question1 = Question('what is osmosis?')
+        self.question2 = Question('What is SSt?')
+
+            
     def test_get_all_questions(self):
-        res = self.app.get('/api/v1.0/items')
+        res = self.app.get('/api/v1/questions')
         self.assertEqual(res.status_code, 200)
         data = json.loads(res.get_data(as_text=True))
         self.assertEqual(len(data['questions']), 2)
 
+    def test_get_question_byId(self):
+        res = self.app.get('/api/v2/questions/2')
+        self.assertEqual(res.status_code, 200)
+        data = json.loads(res.get_data(as_text=True))
+        self.assertEqual(data['question'][0]['id'], 2)
+        
 
 if __name__ == '__main__':
     unittest.main()
