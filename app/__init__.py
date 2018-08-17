273778fa5bbe0bd1b692984d2b9644a17dec622c
faraqhan <f.faraqhan@gmail.com>
2018-08-17 17:16:37 +0300

    create get_all_question_function

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,29 @@
 from flask import Flask, jsonify, request
 
-from model import Question
-
 app = Flask(__name__)
+app.config['TESTING'] = True
+app.testing = True
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
-question_1 = Question('Held up?')
-question_2 = Question('Held up?')
-all_questions = Question.questions
+class Question(object):
 
-@app.route('/api/v1/questions')
+    ques_id = 0
+    questions: list = []
+
+    def __init__(self, name):
+        self.question = {}
+        self.question['name'] = name
+        Question.ques_id += 1
+        self.question['id_no'] = Question.ques_id
+        Question.questions.append(self.question)
+
+    @staticmethod
+    def get_all_questions():
+        return Question.questions
+
+@app.route('/api/v1.0/items', methods=['GET'])
 def get_all_questions():
-    pass
+    return jsonify({'questions' : Question.questions})
 
 if __name__ == '__main__':
     app.run(debug=True)
