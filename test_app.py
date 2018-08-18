af1afcc35c1094aa2f0a2625faad0ba52d6dc3d5
faraqhan <f.faraqhan@gmail.com>
2018-08-18 12:21:12 +0300

    add test units for add question endpoint

--- test_app.py
+++ test_app.py
@@ -9,6 +9,8 @@ class QuestionTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
+        self.question2 = Question()
+        self.question2._add_question('what is your name?')
                     
     def test_get_all_questions(self):
         res = self.app.get('/api/v1/questions')
@@ -17,11 +19,15 @@ class QuestionTestCase(unittest.TestCase):
         self.assertEqual(len(data['questions']), 2)
 
     def test_get_question_byId(self):
-        res = self.app.get('/api/v2/questions/2')
+        res = self.app.get('/api/v2/questions/1')
         self.assertEqual(res.status_code, 200)
         data = json.loads(res.get_data(as_text=True))
-        self.assertEqual(data['question'][0]['id'], 2)
-        
+        self.assertEqual(data['question'][0]['id'], 1)
+
+    def test_add_question(self):
+        result = {'name' : 'reality'}
+        res = self.app.post('/api/v3/questions', data=result)
+        self.assertEqual(res.status_code, 201)
 
 if __name__ == '__main__':
     unittest.main()
--- test_model.py
+++ test_model.py
@@ -4,16 +4,19 @@ from app import Question
 
 class QuestionTest(unittest.TestCase):
 
-    def setUp(self):
-        self.question1 = Question('what is osmosis?')
-        self.question2 = Question('What is SSt?')
-
     def test_get_all_questions(self):
-        self.assertEqual(len(Question._get_all_questions()), 2)
+        self.assertEqual(len(Question._get_all_questions()), 1)
     
     def test_get_question_byId(self):
-        result = Question._get_question_byId(2)
-        self.assertEqual(result[0]['id'], 2)
+        self.question2 = Question()
+        result = Question._get_question_byId(1)
+        self.assertEqual(result[0]['id'], 1)
 
+    def test_add_question(self):
+        self.question2 = Question()
+        result = self.question2._add_question('neuro science')
+        self.assertEqual(self.question2.question['name'], 'neuro science')
+        self.assertEqual(self.question2.question['id'], 1)
+    
 if __name__ == '__main__':
     unittest.main()
