61f1c834d952b79bb849d0dbfeb620ae2f13877e
faraqhan <f.faraqhan@gmail.com>
2018-08-19 08:36:30 +0300

    add test for add answer function in app

--- test_app.py
+++ test_app.py
@@ -9,14 +9,12 @@ class QuestionTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-        self.question2 = Question()
-        self.question2._add_question('what is your name?')
                     
     def test_get_all_questions(self):
         res = self.app.get('/api/v1/questions')
         self.assertEqual(res.status_code, 200)
         data = json.loads(res.get_data(as_text=True))
-        self.assertEqual(len(data['questions']), 2)
+        self.assertEqual(len(data['questions']), 1)
 
     def test_get_question_byId(self):
         res = self.app.get('/api/v2/questions/1')
@@ -27,6 +25,14 @@ class QuestionTestCase(unittest.TestCase):
     def test_add_question(self):
         result = {'name' : 'reality'}
         res = self.app.post('/api/v3/questions', data=result)
+        self.assertEqual(res.status_code, 201)
+
+    def test_add_answer(self):
+        question2 = Question()
+        question2._add_question('neuro science')
+        result = question2._get_question_byId(1)
+        pd = result[0]
+        res = self.app.post('/api/v4/questions/1', data=pd)
         self.assertEqual(res.status_code, 201)
 
 if __name__ == '__main__':
