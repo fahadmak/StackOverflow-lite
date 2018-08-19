e37686e1185fe8ec4771678d852cf17a7ea8225d
faraqhan <f.faraqhan@gmail.com>
2018-08-19 08:35:47 +0300

    add tests for model class in  the app

--- test_model.py
+++ test_model.py
@@ -5,18 +5,27 @@ from app import Question
 class QuestionTest(unittest.TestCase):
 
     def test_get_all_questions(self):
-        self.assertEqual(len(Question._get_all_questions()), 1)
+        question2 = Question()
+        self.assertEqual(len(Question._get_all_questions()), 2)
     
     def test_get_question_byId(self):
-        self.question2 = Question()
+        question2 = Question()
         result = Question._get_question_byId(1)
         self.assertEqual(result[0]['id'], 1)
 
     def test_add_question(self):
-        self.question2 = Question()
-        result = self.question2._add_question('neuro science')
-        self.assertEqual(self.question2.question['name'], 'neuro science')
-        self.assertEqual(self.question2.question['id'], 1)
-    
+        question2 = Question()
+        result = question2._add_question('neuro science')
+        self.assertEqual(question2.question['name'], 'neuro science')
+        self.assertEqual(question2.question['id'], 2)
+
+    def test_add_answer(self):
+        
+        question2 = Question()
+        question2._add_question('neuro science')
+        question2._add_answer(1, 'nemo')
+        result = Question._get_question_byId(1)
+        self.assertEqual(result[0]['answer'], 'nemo')
+        
 if __name__ == '__main__':
     unittest.main()
