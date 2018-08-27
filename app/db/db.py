c7f6891340b134a71f40f65f91e77f1f7c8c569c
faraqhan <f.faraqhan@gmail.com>
2018-08-27 18:50:48 +0300

    create database script

---
+++ app/db/db.py
@@ -0,0 +1,13 @@
+import psycopg2
+from pprint import pprint
+
+class DatabaseConnection:
+    def __init__(self):
+        try:
+            self.connect = psycopg2.connect(
+                "dbname='stack' user='postgres' host='localhost' password='maka1997' port='5432'")
+            self.connect.autocommit = True
+            self.cursor = self.connect.cursor()
+        except:
+            pprint("Cannot connect to database")
+    
